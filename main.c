bf05cf600ef44f638184a14d0dbebaa4457d3edb
Jonathan07200 <jonathantreerhapat.rep@student.mahidol.edu>
2025-09-26 07:39:43 +0000

    test

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 // char_copy: takes an input file, copies it to output file
 int main(void){
     char c;int in,out;
-    
-
+    in = open("file.in",O_RDONLY);
+    out = open("file.out",O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR);
+    while(read(in,&c,1)==1)write(out,&c,1);
 }
+
