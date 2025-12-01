dca8db8582c5dd354c817f41d4bacd45b223f429
Jonathan07200 <jonathantreerhapat.rep@student.mahidol.edu>
2025-12-01 04:35:31 +0000

    copypaste

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 // char_copy: takes an input file, copies it to output file
 int main(void){
-    char c;int in,out;
-    in = open("file.in",O_RDONLY);
-    out = open("file.out",O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR);
-    while(read(in,&c,1)==1)write(out,&c,1);
+    FILE *input, *output; //pointers
+    char ch;
+
+    input = fopen("file.in","r");
+    output = fopen("file.out","w");
+    
+    while ((ch=fgetc(input))!= EOF){ // end of file
+        putc(ch,output);
+    }
+    fclose(input);
+    fclose(output);
+    return 0;
+
 }
 
